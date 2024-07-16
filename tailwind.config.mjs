c2d81167e3876920267eb45b2d4657299b7c0110
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-07-16 12:50:18 +0200

    change md breakpoint to 860px

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      dropShadow: {
+        glow: [
+          "0 0px 20px rgba(255,255, 255, 0.35)",
+          "0 0px 65px rgba(255, 255,255, 0.2)",
+        ],
+      },
+    },
+    screens: {
+      md: "860px",
+    },
+  },
+  plugins: [],
+};
