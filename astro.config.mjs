b98ded0c4520517d236b7639561cd6d4948fec11
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-07-16 16:49:17 +0200

    bunx astro add astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,26 @@
-import {defineConfig} from 'astro/config';
-
+import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import deno from '@deno/astro-adapter';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
-	output: 'server',
-	adapter: deno(),
-	// vite config needed for astro-font
-	vite: {
-		ssr: {
-			external: ['node:buffer', 'node:path', 'node:fs', 'node:os', 'node:crypto'],
-		},
-		resolve: {
-			alias: {path: 'node:path', fs: 'node:fs', os: 'node:os', crypto: 'node:crypto'},
-		},
-	},
+  integrations: [tailwind(), icon()],
+  output: 'server',
+  adapter: deno(),
+  // vite config needed for astro-font
+  vite: {
+    ssr: {
+      external: ['node:buffer', 'node:path', 'node:fs', 'node:os', 'node:crypto']
+    },
+    resolve: {
+      alias: {
+        path: 'node:path',
+        fs: 'node:fs',
+        os: 'node:os',
+        crypto: 'node:crypto'
+      }
+    }
+  }
 });
