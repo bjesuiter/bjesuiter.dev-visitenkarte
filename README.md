65af99dc6ddbde9d21ced21b8dbb9c55ca3d2a9c
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-07-16 17:08:23 +0200

    bun add @iconify-json/bxl   + github icon

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Visitenkarte
 
+## Icon Usage
+
+- Find Icons here: `https://icon-sets.iconify.design/?query=astro`
+  - alternative icon browser: https://icones.js.org/
+- install necessary icon pack, for example: logos:
+  `bun add @iconify-json/logos`
+- use as `<Icon name="logos:github-icon" />`
+
 ## TODOs
 
 - optimize images: https://www.youtube.com/watch?v=kGGnanUKM00
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,39 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import deno from '@deno/astro-adapter';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import deno from "@deno/astro-adapter";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
-  output: 'server',
+  integrations: [
+    tailwind(),
+    icon({
+      include: {
+        bxl: ["github"],
+      },
+    }),
+  ],
+  output: "server",
   adapter: deno(),
   // vite config needed for astro-font
   vite: {
     ssr: {
-      external: ['node:buffer', 'node:path', 'node:fs', 'node:os', 'node:crypto']
+      external: [
+        "node:buffer",
+        "node:path",
+        "node:fs",
+        "node:os",
+        "node:crypto",
+      ],
     },
     resolve: {
       alias: {
-        path: 'node:path',
-        fs: 'node:fs',
-        os: 'node:os',
-        crypto: 'node:crypto'
-      }
-    }
-  }
+        path: "node:path",
+        fs: "node:fs",
+        os: "node:os",
+        crypto: "node:crypto",
+      },
+    },
+  },
 });
