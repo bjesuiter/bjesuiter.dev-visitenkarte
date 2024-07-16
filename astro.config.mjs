6b52a430ffa43affd32852a7367d8ea6c0c6be40
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-07-16 12:15:37 +0200

    include basic visitenkarte component

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,13 @@ export default defineConfig({
 	integrations: [tailwind()],
 	output: 'server',
 	adapter: deno(),
+	// vite config needed for astro-font
+	vite: {
+		ssr: {
+			external: ['node:buffer', 'node:path', 'node:fs', 'node:os', 'node:crypto'],
+		},
+		resolve: {
+			alias: {path: 'node:path', fs: 'node:fs', os: 'node:os', crypto: 'node:crypto'},
+		},
+	},
 });
